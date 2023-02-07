a6ab0f083aff934b5d22792f8a28fae130fc0d19
hermann-web <hermannagossou6@gmail.com>
2023-02-07 13:52:59 +0100

    add code

--- tp-heetch/main.R
+++ tp-heetch/main.R
@@ -2,12 +2,13 @@
 ### Projet d'analyse VTC Heetch ###
 ###################################
 
-# load packages
+# load packages ----
+library(dplyr)
+library(sf)
+library(leaflet)
 
-# load data 
+# load data  ----
+casaBound <- st_read("data/casabound.geojson") #des poinrts qui forment une
+plot(casaBound$geometry)#un plot de la géométrie
 
-# whatever
-
-# whatever
-
-# whatever
+# whatever ----
---
+++ tp-heetch/readme.md
@@ -0,0 +1,13 @@
+# Objective
+
+# Steps
+- download R and R-studio 
+- create a project 
+- install packages
+- download data from [this link](https://acloud.zaclys.com/index.php/s/ZyXcsF2AgGmMKaJ)
+- find the code [here](data/heetch_casablanca.html)
+
+# remember
+atl + - --> def de varible
+TAb --> autocompletet
+ctl ENter --> run
